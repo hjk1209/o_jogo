dd85615c72a6528118f5f3de8c0f2a11c5730306
hjk1209 <93876984+hjk1209@users.noreply.github.com>
2025-11-10 15:44:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,6 @@ app = Flask(__name__)
 # --- CORREÇÃO DE CORS (para Render/Netlify) ---
 origins = [
     "https://guia-kaibora.netlify.app", # O seu site Netlify
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1:5501",
-    "null" # Para testes locais (abrir o ficheiro diretamente)
 ]
 CORS(app, origins=origins, supports_credentials=True)
 
@@ -1001,3 +998,4 @@ if __name__ == '__main__':
     print(f"Banco de dados está em: {os.path.join(base_dir, 'kaibora.db')}")
     print("Acesse o painel do GM em: http://127.0.0.1:5000/gm")
     app.run(debug=True, port=5000)
+
